bbe757d9a41c085a750400a5bd44177b0ae85251
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-03-27 16:19:28 +0400

    Add FilterAttribute

---
+++ Sources/CoreImageFilters/Attribute/FilterAttribute.swift
@@ -0,0 +1,70 @@
+//
+//  Created by Vitali Kurlovich on 26.03.2026.
+//
+
+import CoreImage
+
+public struct FilterAttribute: Hashable {
+    public let filter: CIFilter
+    public let key: String
+}
+
+public extension FilterAttribute {
+    subscript(key: String) -> Any? {
+        attributes[key]
+    }
+}
+
+public extension FilterAttribute {
+    var name: String {
+        self[kCIAttributeName] as? String ?? ""
+    }
+
+    var displayName: String {
+        self[kCIAttributeDisplayName] as? String ?? ""
+    }
+
+    var localizedDescription: String {
+        self[kCIAttributeDescription] as? String ?? ""
+    }
+}
+
+public extension FilterAttribute {
+    var className: String {
+        self[kCIAttributeClass] as? String ?? ""
+    }
+}
+
+extension CIFilter {
+    var inputAttributes: [FilterAttribute] {
+        inputKeys.compactMap {
+            attribute(for: $0)
+        }
+    }
+
+    var outputAttributes: [FilterAttribute] {
+        outputKeys.compactMap {
+            attribute(for: $0)
+        }
+    }
+}
+
+extension FilterAttribute {
+    var type: FilterAttributeType {
+        guard let key = self[kCIAttributeType] as? String else {
+            return .custom
+        }
+        return FilterAttributeType(key)
+    }
+}
+
+extension FilterAttribute {
+    private var attributes: [String: Any] { filter.attributes[key] as? [String: Any] ?? [:] }
+}
+
+private extension CIFilter {
+    func attribute(for key: String) -> FilterAttribute {
+        assert(attributes.keys.contains(key))
+        return FilterAttribute(filter: self, key: key)
+    }
+}
