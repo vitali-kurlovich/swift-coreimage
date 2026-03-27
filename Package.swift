b9a480e18f0285f4245a4d53f0311f287855d3cc
Vitali Kurlovich <vitalikurlovich@gmail.com>
2026-03-27 16:12:38 +0400

    Update Package.swift

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-coreimage",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16),
+        .tvOS(.v16),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // CoreImageUtils
+        
+        .library(
+            name: "CoreImageFilters",
+            targets: ["CoreImageFilters"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CoreImageFilters"
+        ),
+        .testTarget(
+            name: "CoreImageFiltersTests",
+            dependencies: ["CoreImageFilters"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
